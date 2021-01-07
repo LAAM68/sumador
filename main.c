ea1fcba865681053de863aaad9c945f26a36151f
LAAM68 <mla68.go@gmail.com>
2021-01-06 21:04:48 -0600

    Ya lee n enteros de la consola

--- main.c
+++ main.c
@@ -7,7 +7,19 @@
 
 int main(int argc, char **argv)
 {
-	printf("Hola\n");
+	int n, arreglo[100], indice;
+	
+	printf("Hola Mundo\n");
+	printf("Cantidad de numeros a sumar: ");
+	scanf("%d",&n);
+	
+	for(indice = 0; indice<n; indice++)
+	{
+		printf("numero %d: ", indice);
+		scanf("%d",&(arreglo[indice]));
+		//printf("%d\n",arreglo[indice]);
+	}
+	
 	return 0;
 }
 
