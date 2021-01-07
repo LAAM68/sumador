c0756dd941b6f8d8b02701a37828deaa8b8599d0
LAAM68 <mla68.go@gmail.com>
2021-01-06 21:18:10 -0600

    Ya realiza la suma de los 'n' elementos del arreglo

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char **argv)
 {
-	int n, arreglo[100], indice;
+	int n, arreglo[100], indice, suma = 0;
 	
 	printf("Hola Mundo\n");
 	printf("Cantidad de numeros a sumar: ");
@@ -16,7 +16,8 @@ int main(int argc, char **argv)
 	
 	leeArreglo(n,arreglo);
 	
-	
+	for(indice = 0; indice < n; indice++) suma+=arreglo[indice];
+	printf("La suma total es: %d\n", suma);
 	
 	return 0;
 }
